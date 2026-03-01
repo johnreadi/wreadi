b935bbe7bd97293eaae71a3d56f58236b8d66b73
Ton Nom <ton@email.com>
2026-03-01 23:53:05 +0100

    Force 0.0.0.0 by patching server.js at runtime

--- readi/start.sh
+++ readi/start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-export HOSTNAME=0.0.0.0
-unset HOSTNAME
-export HOSTNAME=0.0.0.0
+# Forcer le hostname à 0.0.0.0 en modifiant server.js au runtime
+sed -i "s/process.env.HOSTNAME || '0.0.0.0'/'0.0.0.0'/g" server.js
 node server.js
